d40e82981a2f0781b113d3dc2fb84ce1dd6c4d68
Rafael Ortega Cohen <rafael@MacBook-Air-de-Apple.local>
2020-10-21 08:41:15 -0500

    nuevas mod

--- Scripts/prueba.py
+++ Scripts/prueba.py
@@ -1,2 +1,21 @@
-import paho.mqtt.subscribe as subscribe
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("device/r")
+
+def on_message(client, userdata, msg):
+  if msg.payload.decode() == "Hello world!":
+    print("Yes!")
+  else:
+    print("No!")
+    #client.disconnect()
+    
+client = mqtt.Client()
+client.connect("broker.hivemq.com",1883,60)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
