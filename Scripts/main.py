a80164ba6668155a1719db129e64659bdd0941cf
estebanpc <you@example.com>
2020-10-21 16:07:25 -0500

    EXITO NOJODA

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,25 +1,21 @@
 import paho.mqtt.client as mqtt
 import pandas as pd
 import numpy as np
-import os.path
-import csv
+#import os.path
+#import csv
 
 def Convert(string): 
     li = list(string.split(",")) 
     return li
 
-def search_reader(id):
+def search_reader(yy):
     data=pd.read_csv("readers.csv")
-    r = np.array(data['ID'])
-    id=id.astype(int)
-    print(np.dtype(np.array(id)))
-    
-    for i in r:
-        if np.array(id)==r[i]:
-            print(1999)
-    #if id in r :
-     #   print(True)
-
+    r = data['ID'].tolist()
+    yy = int(yy)
+    if yy in r:
+        print('Ahi esta')
+    else:
+        print('Ahi no esta')
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
@@ -33,8 +29,10 @@ def on_message(client, userdata, msg):
     data1 = data1.T
     data1 = data1.rename(columns={0:"ID",1:"TAG"}) 
     #tag=data1['TAG']
-    id=data1['ID']
-    search_reader(id)
+    xx = np.array(data1['ID'])
+    xx = xx[0]
+    #print(xx)
+    search_reader(xx)
 
 client = mqtt.Client()
 client.connect("broker.hivemq.com",1883,60)
