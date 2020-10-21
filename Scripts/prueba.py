9c29a7038ed18177adcd90852040052b2ef0b8b3
Rafael Ortega Cohen <rafael@MacBook-Air-de-Apple.local>
2020-10-21 09:43:13 -0500

    9:43

--- Scripts/prueba.py
+++ Scripts/prueba.py
@@ -6,16 +6,15 @@ def on_connect(client, userdata, flags, rc):
   client.subscribe("device/r")
 
 def on_message(client, userdata, msg):
-
-	data = str(msg.payload.decode())
-	data = pd.DataFrame(data, columns=['ID', 'TAG']
-    #data = data.to_csv (r'/Users/rafael/Documents/Proyectos/BioInventario\IDTAGS.csv', index = None) 
-    print(data)
- # if msg.payload.decode() == "Hello world!":
- #   print("Yes!")
- # else:
- #   print("No!")
- #   #client.disconnect()
+  data = str(msg.payload.decode())
+  data = pd.DataFrame(data, columns=['ID','TAG'])
+  print(data)
+  #data = data.to_csv (r'/Users/rafael/Documents/Proyectos/BioInventario\IDTAGS.csv', index = None) 
+  # if msg.payload.decode() == "Hello world!":
+  #   print("Yes!")
+  # else:
+  #   print("No!")
+  #   #client.disconnect()
     
 client = mqtt.Client()
 client.connect("broker.hivemq.com",1883,60)
