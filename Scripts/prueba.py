88c5832a21758755d45a749baa2144d2b086d8ab
estebanpc <you@example.com>
2020-11-16 17:56:50 -0500

    FINALV0.8.5E

--- Scripts/prueba.py
+++ Scripts/prueba.py
@@ -13,9 +13,9 @@ def Convert(string):
 '''Esta función sera la que generara el archivo de localizacion principal
    y tendra el trabajo de presentar en pantalla la evolucion de dicho archivo
    en tiempo real'''
-def display(u):
+def display(data_gen):
     if os.path.isfile('display.csv')==False:
-        data = pd.DataFrame(u) 
+        data = pd.DataFrame(data_gen) 
         data = data.T
         data = data.rename(columns={0:"TAG",1:"MDEVICE",2:"LOCATION",3:"IN_TIME"})
         os.system('clear')
@@ -23,30 +23,30 @@ def display(u):
         data.to_csv(r'display.csv', index = False)
     elif os.path.isfile("display.csv")==True:       
         dat = pd.read_csv("display.csv")
-        h = np.array(u["TAG"])
-        j = np.array(dat["TAG"])
-        if h in j:
+        tags_gen = np.array(data_gen["TAG"])
+        tags_act = np.array(dat["TAG"])
+        if tags_gen in tags_act:
             os.system('clear')
-            i=list(dat[dat['TAG']==u['TAG']].index)
-            k =  dat.drop(dat.index[i]).reset_index().drop(columns='index')
-            print(k)
-            k.to_csv(r'display.csv', index = False)
+            index = list(dat[dat['TAG']==data_gen['TAG']].index)
+            data_act =  dat.drop(dat.index[index]).reset_index().drop(columns='index')
+            print(data_act)
+            data_act.to_csv(r'display.csv', index = False)
         else:
-            data1 = pd.DataFrame(u)
-            data1 = data1.T
-            data1 = data1.rename(columns={0:"TAG",1:"MDEVICE",2:"LOCATION",3:"IN_TIME"})
-            k = pd.DataFrame(pd.concat([data1, dat])).reset_index().drop(columns='index')
+            data_proc = pd.DataFrame(data_gen)
+            data_proc = data_proc.T
+            data_proc = data_proc.rename(columns={0:"TAG",1:"MDEVICE",2:"LOCATION",3:"IN_TIME"})
+            data_act = pd.DataFrame(pd.concat([data_proc, dat])).reset_index().drop(columns='index')
             os.system('clear')
-            print(k)
-            k.to_csv(r'display.csv', index = False)
+            print(data_act)
+            data_act.to_csv(r'display.csv', index = False)
 
 ''' Esta Funcion es la encargada guardar historial de movimientos de los equipos medicos'''
 def save_history(tag, medevice, location):
-    yy = int(tag)
+    tag = int(tag)
     if os.path.isfile("hdevice.csv")==False: 
         time = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-        tucu=[yy, medevice, location, time]
-        data = pd.DataFrame(tucu)
+        table_data=[tag, medevice, location, time]
+        data = pd.DataFrame(table_data)
         data = data.T
         data = data.rename(columns={0:"TAG",1:"MDEVICE",2:"LOCATION",3:"IN_TIME"})
         os.system('clear')
@@ -55,76 +55,76 @@ def save_history(tag, medevice, location):
     elif os.path.isfile("hdevice.csv")==True:        
         dat = pd.read_csv("hdevice.csv")
         time = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-        tucu = [yy, medevice, location, time]
-        data1 = pd.DataFrame(tucu)
-        data1 = data1.T
-        data1 = data1.rename(columns={0:"TAG",1:"MDEVICE",2:"LOCATION",3:"IN_TIME"})
-        u = pd.DataFrame(pd.concat([data1, dat])).reset_index().drop(columns='index')
+        table_data = [tag, medevice, location, time]
+        data_proc = pd.DataFrame(table_data)
+        data_proc = data_proc.T
+        data_proc = data_proc.rename(columns={0:"TAG",1:"MDEVICE",2:"LOCATION",3:"IN_TIME"})
+        data_gen = pd.DataFrame(pd.concat([data_proc, dat])).reset_index().drop(columns='index')
         os.system('clear')
-        display(u.iloc[0])
-        #print(u)
-        u.to_csv(r'hdevice.csv', index = False)    
+        display(data_gen.iloc[0])
+        #print(data_gen)
+        data_gen.to_csv(r'hdevice.csv', index = False)    
 
 ''' Esta Funcion es la encargada de crear la base de datos de dispositivos medicos'''
-def search_mdevice(yy, location):
-    yy = int(yy)
+def search_mdevice(tag, location):
+    tag = int(tag)
     if os.path.isfile("mdevice.csv")==False: 
-        print('Digite el nombre del nuevo dispositivo medico con TAG: ', yy)
+        print('Digite el nombre del nuevo dispositivo medico con TAG: ', tag)
         loc=input()
-        tucu=[yy, loc]
-        data = pd.DataFrame(tucu)
+        table_data=[tag, loc]
+        data = pd.DataFrame(table_data)
         data = data.T
         data = data.rename(columns={0:"TAG",1:"MDEVICE"})
         print(data)
         data.to_csv(r'mdevice.csv', index = False)
     elif os.path.isfile("mdevice.csv")==True:
         dat = pd.read_csv("mdevice.csv")
-        r = dat['TAG'].tolist()
-        t = dat.where(dat['TAG']==yy).dropna()
-        t1 = t['MDEVICE'].tolist()
-        if yy in r:
-            save_history(yy, t1[0], location)
+        tag_list = dat['TAG'].tolist()
+        tag_finder = dat.where(dat['TAG']==tag).dropna()
+        dev_finder = tag_finder['MDEVICE'].tolist()
+        if tag in tag_list:
+            save_history(tag, dev_finder[0], location)
         else:
-            print('Digite el nombre del nuevo dispositivo medico con TAG: ', yy)
+            print('Digite el nombre del nuevo dispositivo medico con TAG: ', tag)
             loc=input()
-            tucu=[yy, loc]
-            data1 = pd.DataFrame(tucu)
-            data1 = data1.T
-            data1 = data1.rename(columns={0:"TAG",1:"MDEVICE"})
-            u=dat.append(data1, ignore_index=True)
-            print(u)
-            u.to_csv(r'mdevice.csv', index = False)
+            table_data=[tag, loc]
+            data_proc = pd.DataFrame(table_data)
+            data_proc = data_proc.T
+            data_proc = data_proc.rename(columns={0:"TAG",1:"MDEVICE"})
+            data_gen=dat.append(data_proc, ignore_index=True)
+            print(data_gen)
+            data_gen.to_csv(r'mdevice.csv', index = False)
 
 ''' Esta Funcion es la encargada de crear la base de datos de dispositivos lectores RFID'''
-def search_reader(yy, zz):
-    zz=int(zz)
-    yy = int(yy)
+def search_reader(ids, tag):
+    tag = int(tag)
+    ids = int(ids)
     if os.path.isfile("readersl.csv")==False: 
-        print('Digite la ubicación del nuevo lector SERIAL: ', yy)
-        loc=input()
-        tucu=[yy, loc]
-        data = pd.DataFrame(tucu)
+        print('Digite la ubicación del nuevo lector SERIAL: ', ids)
+        loc = input()
+        table_data = [ids, loc]
+        data = pd.DataFrame(table_data)
         data = data.T
         data = data.rename(columns={0:"SERIAL",1:"LOC"})
         print(data)
         data.to_csv(r'readersl.csv', index = False)
     elif os.path.isfile("readersl.csv")==True:
         dat = pd.read_csv("readersl.csv")
-        r = dat['SERIAL'].tolist()
-        t = dat.where(dat['SERIAL']==yy).dropna()
-        t1 = t['LOC'].tolist()
-        if yy in r:
-           search_mdevice(zz, t1[0])
+        ids_list = dat['SERIAL'].tolist()
+        ids_finder = dat.where(dat['SERIAL']==ids).dropna()
+        loc_finder = ids_finder['LOC'].tolist()
+        if ids in ids_list:
+           search_mdevice(tag, loc_finder[0])
         else:
-            print('Digite la ubicación del nuevo lector: ', yy)
+            print('Digite la ubicación del nuevo lector: ', ids)
             loc=input()
-            tucu=[yy, loc]
-            data1 = pd.DataFrame(tucu)
-            data1 = data1.T
-            data1 = data1.rename(columns={0:"SERIAL",1:"LOC"})
-            u=dat.append(data1, ignore_index=True)
-            print(u)
-            u.to_csv(r'readersl.csv', index = False)
+            table_data=[ids, loc]
+            data_proc = pd.DataFrame(table_data)
+            data_proc = data_proc.T
+            data_proc = data_proc.rename(columns={0:"SERIAL",1:"LOC"})
+            data_gen=dat.append(data_proc, ignore_index=True)
+            print(data_gen)
+            data_gen.to_csv(r'readersl.csv', index = False)
 
 ''' Esta Funcion es la encargada de suscribir la aplicación en el tópico device/r dentro del blocker'''
 def on_connect(client, userdata, flags, rc):
@@ -133,17 +133,17 @@ def on_connect(client, userdata, flags, rc):
 ''' Esta Funcion es la encargada de recibir los mensajes que envían los rectores al tópico device/r dentro del blocker.
 Tambien envía dicha información a las funciones de registro de lectores y dispositivos medicos'''
 def on_message(client, userdata, msg):
-    d = msg.payload.decode()
-    d=Convert(d)
-    z = np.array(d)
-    data1 = pd.DataFrame(z)
-    data1 = data1.T
-    data1 = data1.rename(columns={0:"SERIAL",1:"TAG"}) 
-    xx = np.array(data1['SERIAL'])
-    xx = xx[0]
-    tag = np.array(data1['TAG'])
+    read_broq = msg.payload.decode()
+    read_broq = Convert(read_broq)
+    read_array = np.array(read_broq)
+    data_proc = pd.DataFrame(read_array)
+    data_proc = data_proc.T
+    data_proc = data_proc.rename(columns={0:"SERIAL",1:"TAG"}) 
+    ids = np.array(data_proc['SERIAL'])
+    ids = ids[0]
+    tag = np.array(data_proc['TAG'])
     tag = tag[0]
-    search_reader(xx, tag)
+    search_reader(ids, tag)
 
 
 client = mqtt.Client() #Utilizamos la case cliente para crear la instancia mqtt
