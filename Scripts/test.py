9cece5bffb9795268d4d2ca819cf7628fe0ea4f5
raoc <rafael.ortega.cohen@gmail.com>
2021-04-06 22:29:14 -0500

    Delete test.py

--- Scripts/test.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('persons.csv', 'wb') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['Name', 'Profession'])
-    filewriter.writerow(['Derek', 'Software Developer'])
-    filewriter.writerow(['Steve', 'Software Developer'])
-    filewriter.writerow(['Paul', 'Manager'])
